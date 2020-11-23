addb2f486d80a2883d5c0722819692e9fa5b3857
pyagmyrov <permanyagmyrov@gmail.com>
2020-11-23 22:01:30 +0200

    debug mode false

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'x-=x1=^q%lei&%c(go1s&pk_3_-!-#@_w5vt_5-j&eg9i6#+sf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['165.22.1.20',]
 
