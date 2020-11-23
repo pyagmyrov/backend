84d74243786e5e1c5b1004338c3b451227a32374
pyagmyrov <permanyagmyrov@gmail.com>
2020-11-23 23:51:50 +0200

    edited allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x-=x1=^q%lei&%c(go1s&pk_3_-!-#@_w5vt_5-j&eg9i6#+sf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['165.22.1.20',]
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
