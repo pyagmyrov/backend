dd33493517f54df8ec89b90ff45bd538444e2176
Perman Yagmyrov <permanyagmyrov@gmail.com>
2020-11-25 14:43:56 +0200

    Update setup.sh

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repoo.
-PROJECT_GIT_URL= 'https://github.com/pyagmyrov/backend.git'
+PROJECT_GIT_URL='https://github.com/pyagmyrov/backend.git'
 
 PROJECT_BASE_PATH='/usr/local/apps/backend'
 
